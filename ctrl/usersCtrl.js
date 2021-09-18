921e7d1ba9ec2535fc877b229aa75a9b894884a9
Hodaia Efergan <hodaia0524289665@gmail.com>
2021-09-19 00:11:39 +0300

    start

--- ctrl/usersCtrl.js
+++ ctrl/usersCtrl.js
@@ -2,7 +2,7 @@ const User = require('../model/user.model');
 
 module.exports.register = async (req, res) => {
     try {
-
+//h
         const email = req.body.email;
         const password = req.body.password;
         //const name = req.body.name || '';
